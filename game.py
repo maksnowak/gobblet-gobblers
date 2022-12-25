e1ec0af539418ef33e9491776c88bd09bb3754df
maksnowak <59841779+maksnowak@users.noreply.github.com>
2022-12-25 16:01:01 +0100

    Add the players' pieces

--- game.py
+++ game.py
@@ -3,9 +3,29 @@ class Game:
         self.__board = []
         for _ in range(size):
             self.__board.append([[]] * size)
+        self.__player = []
+        self.__opponent = []
+        for i in range(size):
+            # TODO refactor creating the player's pieces so that the instructions aren't repeated
+            self.__player.append(('player', i + 1))
+            self.__player.append(('player', i + 1))
+            self.__opponent.append(('opponent', i + 1))
+            self.__opponent.append(('opponent', i + 1))
 
     def board(self):
         return self.__board
 
     def set_board(self, new_board):
         self.__board = new_board
+
+    def players_pieces(self):
+        return self.__player
+
+    def set_players_pieces(self, new_pieces):
+        self.__player = new_pieces
+
+    def opponents_pieces(self):
+        return self.__opponent
+
+    def set_opponents_pieces(self, new_pieces):
+        self.__opponent = new_pieces
--- test_game.py
+++ test_game.py
@@ -33,3 +33,37 @@ def test_game_set_board():
         [[4], [5], [6]],
         [[7], [8], [9]]
     ]
+
+
+def test_game_players_pieces():
+    game = Game(3)
+    assert game.players_pieces() == [
+        ('player', 1), ('player', 1), ('player', 2), ('player', 2), ('player', 3), ('player', 3)
+    ]
+
+
+def test_game_opponents_pieces():
+    game = Game(3)
+    assert game.opponents_pieces() == [
+        ('opponent', 1), ('opponent', 1), ('opponent', 2), ('opponent', 2), ('opponent', 3), ('opponent', 3)
+    ]
+
+
+def test_game_set_players_pieces():
+    game = Game(3)
+    game.set_players_pieces([
+        ('player', 1), ('player', 3)
+    ])
+    assert game.players_pieces() == [
+        ('player', 1), ('player', 3)
+    ]
+
+
+def test_game_set_opponents_pieces():
+    game = Game(3)
+    game.set_opponents_pieces([
+        ('opponent', 1), ('opponent', 3)
+    ])
+    assert game.opponents_pieces() == [
+        ('opponent', 1), ('opponent', 3)
+    ]
