544018e9c5d2e9f0f27832f69f9d9376c110bd1a
maksnowak <59841779+maksnowak@users.noreply.github.com>
2023-01-02 12:00:34 +0100

    Add main menu

--- main.py
+++
@@ -1,9 +0,0 @@
-# from game import Game
-
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
---
+++ ui.py
@@ -0,0 +1,19 @@
+# from game import Game
+
+
+def menu():
+    print('''
+Witaj w grze Gobblet Gobblers!
+
+1) Gra z drugim graczem
+2) Gra z komputerem
+    ''')
+    input('Wybierz jedną z powyższych opcji wpisując jej numer... ')
+
+
+def main():
+    menu()
+
+
+if __name__ == '__main__':
+    main()
