a2947de9618ab54e263f0f3c2ad8d78bdecb3c34
maksnowak <59841779+maksnowak@users.noreply.github.com>
2023-01-05 12:20:41 +0100

    Add error handling in game view (kind of)

--- main.py
+++ main.py
@@ -20,14 +20,50 @@ Witaj w grze Gobblet Gobblers!
 def game(size, ai):
     game = Game(size)
     ui = Interface(game, ai)
-    turn = 1
+    _round = 1
     error_output = ''
+    move_prompt = 'Ruch gracza{player}. Czy chcesz postawić nowy pionek? [T]ak/[N]ie: '
+    size_prompt = 'Podaj rozmiar pionka: '
+    position_prompt = 'Podaj koordynaty pionka, który chcesz przenieść (najpierw podaj numer kolumny, a następnie wiersza): '
+    new_position_prompt = 'Podaj koordynaty, na które chcesz postawić pionek (najpierw podaj numer kolumny, a następnie wiersza): '
+
+    def move(player):
+        nonlocal error_output
+        new_piece = input(move_prompt.format(player=''))[0].upper()
+        if new_piece == 'T':
+            piece_size = input(size_prompt)
+            if not piece_size.isnumeric() or int(piece_size) > size:
+                error_output = 'Nieprawidłowy rozmiar pionka'
+                return
+            new_coordinates = input(new_position_prompt).split()[:2]
+            if any([int(coordinate) <= 0 or int(coordinate) > size for coordinate in new_coordinates]):
+                error_output = 'Nieprawidłowe koordynaty'
+        elif new_piece == 'N':
+            coordinates = input(position_prompt).split()[:2]
+            if any([int(coordinate) <= 0 or int(coordinate) > size for coordinate in coordinates]):
+                error_output = 'Nieprawidłowe koordynaty'
+                return
+            new_coordinates = input(new_position_prompt).split()[:2]
+            if any([int(coordinate) <= 0 or int(coordinate) > size for coordinate in new_coordinates]):
+                error_output = 'Nieprawidłowe koordynaty'
+                return
+        else:
+            error_output = 'Nieprawidłowy wybór'
+            return
     while True:
+        os.system('clear')
         print(ui.board())
         print(ui.pieces('player_one'))
         print(ui.pieces('player_two'))
         print(error_output)
-        break
+        if ai and _round % 2 == 1:
+            move('')
+        elif ai:
+            pass
+        elif _round % 2 == 1:
+            move(' 1')
+        else:
+            move(' 2')
 
 
 def main():
@@ -42,7 +78,6 @@ def main():
             message = ''
             size = input('Podaj rozmiar planszy: ')
             if size.isnumeric() and int(size) > 2:
-                os.system('clear')
                 game(int(size), False)
                 break
             else:
